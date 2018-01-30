1ac0fe8458fa235a5b004acd256592c92a579dc8
Brian McGee <b.m.mcgee@gmail.com>
2018-01-30 11:09:58 +0100

    Refactored to make s3 config more generic

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,15 +16,23 @@ S3_ENCRYPTION_KEY="${S3_ENCRYPTION_KEY-no}"
 [ "${S3_SECRET_KEY}" != 'no' ] &&
 [ "${S3_ENCRYPTION_KEY}" != 'no' ]
 
-S3_CONFIGURED=$?
+[[ $S3_BUCKET ]] && [[ $S3_PATH ]] && [[ $S3_ACCESS_KEY ]] && [[ $S3_SECRET_KEY ]] && [[ $S3_ENCRYPTION_KEY ]]
 
-if [ "${S3_CONFIGURED}" -eq 0 ]; then
+if [ "$?" -eq 0 ]; then
 
     echo "Configuring backup"
 
-    echo "access_key=${S3_ACCESS_KEY}" >> /root/.s3cfg
-    echo "secret_key=${S3_SECRET_KEY}" >> /root/.s3cfg
-    echo "gpg_passphrase=${S3_ENCRYPTION_KEY}" >> /root/.s3cfg
+    CONFIG_FILE="/root/.s3cfg"
+
+    sed -i "/access_key = .*/c\access_key = ${S3_ACCESS_KEY}" ${CONFIG_FILE}
+    sed -i "/secret_key = .*/c\secret_key = ${S3_SECRET_KEY}" ${CONFIG_FILE}
+    sed -i "/gpg_passphrase = .*/c\gpg_passphrase = ${S3_ENCRYPTION_KEY}" ${CONFIG_FILE}
+
+    # optional parameters
+
+    [[ $S3_HOST_BASE ]] && sed -i "/host_base = .*/c\host_base = ${S3_HOST_BASE}" ${CONFIG_FILE}
+    [[ $S3_HOST_BUCKET ]] && sed -i "/host_bucket = .*/c\host_bucket = ${S3_HOST_BUCKET}" ${CONFIG_FILE}
+
 
     # weekly backup (on sundays, but not the first day of the month)
 
